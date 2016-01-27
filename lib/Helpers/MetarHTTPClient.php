577ddab75b34a1882ccc9dcd9d5bdc1a211545b3
Bobby Allen <ballen@bobbyallen.me>
2016-01-27 21:11:37 +0000

    Adds unit test for the MetarHTTPClient and tests a mock response object.

--- lib/Helpers/MetarHTTPClient.php
+++ lib/Helpers/MetarHTTPClient.php
@@ -20,16 +20,37 @@ use \Exception;
 class MetarHTTPClient
 {
 
+    /**
+     * Optional Guzzle Client configuration.
+     * @var array
+     */
+    protected $guzzle_conf;
+
+    /**
+     * HTTP Client
+     * @param array $config Optional Guzzle configuration.
+     */
+    public function __construct($config = [])
+    {
+        $this->guzzle_conf = $config;
+    }
+
+    /**
+     * Make a HTTP request and retrieve the body.
+     * @param string $url The URL to request
+     * @return string
+     * @throws Exception
+     */
     public function getMetarAPIResponse($url)
     {
-        $client = new HttpClient();
+        $client = new HttpClient($this->guzzle_conf);
         try {
             $response = $client->get($url);
             if ($response->getStatusCode() != 200) {
                 throw new Exception('An error occured when attempting to access the remote webservice, please try again shortly!');
             }
         } catch (Exception $ex) {
-            die('An exception was caught: ' . $ex->getMessage());
+            die('An exception was caught: ' . $ex->getMessage('http://metar.vatsim.net/metar.php?id=EGSS'));
         }
         return $response->getBody();
     }
--- tests/MetarHttpClientTest.php
+++ tests/MetarHttpClientTest.php
@@ -1,7 +1,11 @@
 <?php
 use \PHPUnit_Framework_TestCase;
-
-
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Metar
@@ -17,5 +21,40 @@ use \PHPUnit_Framework_TestCase;
  */
 class MetarHttpClientTest extends PHPUnit_Framework_TestCase
 {
+
+    /**
+     * Example report data that we expect to recieve.
+     */
+    const EXAMPLE_METAR_REPORT = 'EGSS 272020Z AUTO 27009KT 240V310 9999 BKN044 08/04 Q1010';
     
+    /**
+     * Example service URL
+     */
+    const VATSIM_SERVICE_URL = "http://metar.vatsim.net/metar.php?id=%s";
+
+    /**
+     * The Mock Handler for the Guzzle Client.
+     * @var HandlerStack
+     */
+    private $handler;
+
+    /**
+     * Setup the testcase
+     */
+    public function setUp()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], self::EXAMPLE_METAR_REPORT),
+        ]);
+        $this->handler = HandlerStack::create($mock);
+    }
+
+    /**
+     * Test recieving a METAR report.
+     */
+    public function testDownloadReport()
+    {
+        $client = new Ballen\Metar\Helpers\MetarHTTPClient(['handler' => $this->handler]);
+        $this->assertEquals(self::EXAMPLE_METAR_REPORT, $client->getMetarAPIResponse(sprintf(self::VATSIM_SERVICE_URL, 'EGSS')));
+    }
 }
